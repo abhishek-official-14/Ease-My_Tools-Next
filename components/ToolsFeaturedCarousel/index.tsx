185ab53d9c5e7d5b4608807a4b211c6efab57168
Pranshu Sinha <pranshu.yashuuma@gmail.com>
2026-07-31 20:00:50 +0530

    checkpoint all tools button added

--- components/ToolsFeaturedCarousel/index.tsx
+++ components/ToolsFeaturedCarousel/index.tsx
@@ -5,6 +5,8 @@ import Header from "@/components/Header"
 import ToolSearch from "./ToolSearch"
 import CategoryCarousel from "./CategoryCarousel"
 import { getFeaturedCategories } from "@/data/featuredCategories"
+import { Button } from "../ui/button"
+import Link from "next/link"
 
 export default function ToolsPage() {
     const [searchQuery, setSearchQuery] = useState("")
@@ -14,11 +16,16 @@ export default function ToolsPage() {
         <>
             <Header />
             <main className="bg-background p-4 text-foreground transition-colors">
-                {/* <ToolSearch
+                <ToolSearch
                     searchQuery={searchQuery}
                     setSearchQuery={setSearchQuery}
-                /> */}
+                />
                 <CategoryCarousel featuredCategories={featuredCategories} />
+                <div className="flex justify-center mt-6 text-base">
+                    <Button variant={"default"}>
+                        <Link href={"/tools"}>All Tools →</Link>
+                    </Button>
+                </div>
             </main>
         </>
     )
